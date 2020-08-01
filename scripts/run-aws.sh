0fe5a15218575acaef4868022b9f54b9ff3c91f6
Benyamin <beneslami72@gmail.com>
2020-08-01 15:42:20 +0430

    change to support i3.large instance

--- scripts/run-aws.sh
+++ scripts/run-aws.sh
@@ -9,7 +9,7 @@ tcmalloc="env LD_PRELOAD=${HOME}/gperftools/.libs/libtcmalloc.so "
 #Run YCSB A B C (D = A, F =B)
 #4 workers per disk, 4 load injectors
 #
-rm -f /scratch*/kvell/*
+rm -f /scratch0/kvell/*
 
 cp ${mainDir}/main.c ${mainDir}/main.c.bak
 cat ${mainDir}/main.c | perl -pe 's://.nb_load_injectors = 4:.nb_load_injectors = 4:' | perl -pe 's:[^/].nb_load_injectors = 12: //.nb_load_injectors = 12:' | perl -pe 's:[^/]ycsb_e_uniform,: //ycsb_e_uniform,:' | perl -pe 's://ycsb_a_uniform,:ycsb_a_uniform,:' | perl -pe 's://ycsb_a_zipfian,:ycsb_a_zipfian,:' > ${mainDir}/main.c.tmp
@@ -17,10 +17,10 @@ mv ${mainDir}/main.c.tmp ${mainDir}/main.c
 make -C ${mainDir} -j
 
 echo "Run 1"
-${tcmalloc} ${mainDir}/main 8 4 | tee log_ycsb_1
+${tcmalloc} ${mainDir}/main 1 1 | tee log_ycsb_1
 
 echo "Run 2"
-${tcmalloc} ${mainDir}/main 8 4 | tee log_ycsb_2
+${tcmalloc} ${mainDir}/main 1 1 | tee log_ycsb_2
 
 mv ${mainDir}/main.c.bak ${mainDir}/main.c
 
@@ -28,7 +28,7 @@ mv ${mainDir}/main.c.bak ${mainDir}/main.c
 #Run YCSB E
 #3 workers per disk, 12 load innjectors
 #
-rm -f /scratch*/kvell/* # Change in # workers, reset DB
+rm -f /scratch0/kvell/* # Change in # workers, reset DB
 
 cp ${mainDir}/main.c ${mainDir}/main.c.bak
 cat ${mainDir}/main.c | perl -pe 's://.nb_load_injectors = 12:.nb_load_injectors = 12:' | perl -pe 's:[^/].nb_load_injectors = 4: //.nb_load_injectors = 4:' | perl -pe 's://ycsb_e_uniform, y:ycsb_e_uniform, y:' | perl -pe 's:[^/]ycsb_a_uniform,: //ycsb_a_uniform,:' | perl -pe 's:[^/]ycsb_a_zipfian,: //ycsb_a_zipfian,:' > ${mainDir}/main.c.tmp
@@ -36,10 +36,10 @@ cp ${mainDir}/main.c.tmp ${mainDir}/main.c
 make -C ${mainDir} -j
 
 echo "Run 1 (scans)"
-${tcmalloc} ${mainDir}/main 8 3 | tee log_ycsb_e_1
+${tcmalloc} ${mainDir}/main 1 1 | tee log_ycsb_e_1
 
 echo "Run 2 (scans)"
-${tcmalloc} ${mainDir}/main 8 3 | tee log_ycsb_e_2
+${tcmalloc} ${mainDir}/main 1 1 | tee log_ycsb_e_2
 
 mv ${mainDir}/main.c.bak ${mainDir}/main.c
 
